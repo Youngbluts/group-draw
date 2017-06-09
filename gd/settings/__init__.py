3b88ba81b226f205fbfaa5d0c02c9a9f7f2f4e03
byoungb <by@youngmontee.com>
2017-06-08 20:37:38 -0500

    Initial django project with channels

---
+++ gd/settings/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from gd.settings.local import *
+except ImportError:
+    from gd.settings.base import *
---
+++ gd/settings/base.py
@@ -0,0 +1,64 @@
+from os.path import join, dirname, abspath
+
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
+
+SECRET_KEY = 'xq3aw0@ico%(1()bw_ttqu1&ayfvzt==bclbvan)7ai7r&upe6'
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '*',
+]
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'channels',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+ROOT_URLCONF = 'gd.urls'
+
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
+}]
+
+WSGI_APPLICATION = 'gd.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/Chicago'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
